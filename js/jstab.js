a4fbf19a43ea30322ba72710f33db091835cb8ba
devilthemes.com maharja <devilthemes@gmail.com>
2018-03-12 08:39:32 +0545

    combine 3 function into one

--- js/jstab.js
+++ js/jstab.js
@@ -1,32 +1,49 @@
 const TabControl=function(myTab){
-		this.myTab=document.querySelectorAll(myTab)[0];
 		
-		this.init=function(){
-			for(let i=0; i<this.myTab.children.length; i++){
-				if(this.myTab.children[i].nodeName=="NAV"){
-					let li=this.myTab.childNodes[i].nextElementSibling.children[0].children;
+		
+		this.init=function(myTab){
+			var myTab=document.querySelectorAll(myTab)[0];
+			this.loadData=this.processTab.bind(myTab);
+			for(let i=0; i<myTab.children.length; i++){
+				if(myTab.children[i].nodeName=="NAV"){
+					let li=myTab.childNodes[i].nextElementSibling.children[0].children;
 					
 					for(let j=0; j<li.length; j++){
 						let alink=li[j].children[0];
-						let mydata=this.goTarget.bind(this);
-						alink.addEventListener("click",mydata,false)
+						
+						alink.addEventListener("click",this.loadData,false)
 					}
 					
 				}
 			}
-			window.addEventListener("hashchange",this.setTabClass,false);
-			let loadData=this.winLoadSetTabClass.bind(this.myTab);
-			window.addEventListener("load",loadData,false);
-		}
-		this.winLoadSetTabClass=function(event){
+			window.addEventListener("hashchange",this.loadData,false);
 			
-			myTab=document.querySelectorAll(myTab)[0];
-			var eventhash=event.target.location.href;
+			window.addEventListener("load",this.loadData,false);
+			
+		}
+		
+		this.processTab=function(event){
+			console.log("event");
+			var myTab=document.querySelectorAll("#myTab")[0];
+			var eventhash="";
+			// Event : load
+			if(event.type=="load"){
+				eventhash=event.target.location.href;
 			 // Stop if can not found hash on location
 			 if(eventhash.search("#")==-1){
 				return false;
 			 }
 			 eventhash=eventhash.split("#");
+			}
+			// Event : hashChange
+			if(event.type=="hashchange"){
+				if(event.newURL.search("#")==-1){
+					return false;
+				}
+				eventhash=event.newURL.split("#");
+			}
+			
+			// Tab Button
 			for(let i=0; i<myTab.children.length; i++){
 				if(myTab.children[i].nodeName=="NAV"){
 					let li=myTab.childNodes[i].nextElementSibling.children[0].children;
@@ -39,9 +56,11 @@ const TabControl=function(myTab){
 						// Setting Active Class on A tag
 						if(alinkHash[1]==eventhash[1]){
 							alink.className="active";
+							alink.parentElement.className="activeLi";
 							
 						}else{
 							alink.className="";
+							alink.parentElement.className="";
 						}
 						
 						
@@ -49,7 +68,7 @@ const TabControl=function(myTab){
 					}
 					
 				}
-				// Find div.tabs and set activeTab class 
+				// Tab Content
 				if(myTab.children[i].nodeName=="DIV" && myTab.children[i].className=="tabs"){
 					let divTab=myTab.children[i];
 					
@@ -66,71 +85,8 @@ const TabControl=function(myTab){
 			}
 			
 		}
-		this.setTabClass=function(event){
-			//console.log(event.newURL);
-			if(event.newURL.search("#")==-1){
-				return false;
-			}
-			
-			var eventhash=event.newURL.split("#");
-			
-			for(let i=0; i<this.myTab.children.length; i++){
-				if(this.myTab.children[i].nodeName=="NAV"){
-					let li=this.myTab.childNodes[i].nextElementSibling.children[0].children;
-					
-					for(let j=0; j<li.length; j++){
-						let alink=li[j].children[0];
-						let aLinkHref=alink.href;
-						let alinkHash=aLinkHref.split("#");
-						
-						// Setting Active Class on A tag
-						if(alinkHash[1]==eventhash[1]){
-							alink.className="active";
-						}else{
-							alink.className="";
-						}
-						
-						
-						
-					}
-					
-				}
-				// Find div.tabs and set activeTab class 
-				if(this.myTab.children[i].nodeName=="DIV" && this.myTab.children[i].className=="tabs"){
-					let divTab=this.myTab.children[i];
-					
-					for(let i=0; i<divTab.children.length; i++){
-						console.log(divTab.children[i]);
-						if(divTab.children[i].id==eventhash[1]){
-							divTab.children[i].setAttribute("class","tab activeTab");
-						}else{
-							console.log("Not Found");
-							divTab.children[i].setAttribute("class","tab");
-						}
-					}
-				}
-			}
-		}
-		this.goTarget=function(event){
-			
-			let eachLink=event.target.parentElement.parentElement.children;
-			// Removing all class from a links
-			for(let i=0; i<eachLink.length; i++){
-				eachLink[i].children[0].className="";
-			}
-			// Set Active Class on target link
-			event.target.className="active";
-			
-			let link=event.target.hash.split("#");
-			let myTab=document.querySelectorAll("#myTab")[0];
-			
-			for(var i=0;i<myTab.children[1].children.length;i++){
-				myTab.children[1].children[i].className="tab";
-				if(myTab.children[1].children[i].id==link[1]){
-					myTab.children[1].children[i].className="tab activeTab";
-				}else{
-					myTab.children[1].children[i].className="tab";
-				}
-			}
-		}
+		
+
+		
+		
 	}
